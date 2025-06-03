1a26d766269ec31a13f7e5b09fe5beb6b21955ac
Hengyi <hengyi2507@gmail.com>
2025-06-04 01:07:08 +0200

    Todo listo, solo falta que la 2da grabación este en un frame distinto

--- visualizar.sh
+++ visualizar.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+
+# Limpiamos procesos en segundo plano
+pkill -f "rosbag play"
+pkill -f static_transform_publisher 
+pkill -f nodelet
+pkill -f rviz
+sleep 1
+
 cd rosbag_recordings
 
 # 1) Todos los nodos usarán el reloj del bag
 rosparam set use_sim_time true
 
-# 2) Transformaciones estáticas (6 números = x y z yaw pitch roll)
-rosrun tf2_ros static_transform_publisher 0 0 0  0 0 0   camera_link cam1_link &
-rosrun tf2_ros static_transform_publisher 1 0 0  3.14159 0 0  camera_link cam2_link &
+# 2) Publicamos las transformaciones estáticas
+rosrun tf2_ros static_transform_publisher 0 0 0 0 0 -1.5708 \
+       camera_link cam1_link   __name:=tf_cam1 &
+rosrun tf2_ros static_transform_publisher 1 0 0 3.1416 0 -1.5708 \
+       camera_link cam2_link   __name:=tf_cam2 &
 
-rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0  camera_link camera_depth_optical_frame &
-rosrun tf2_ros static_transform_publisher 1 0 0 3.14159 0 0  camera_link camera2_depth_optical_frame &
+rosrun tf2_ros static_transform_publisher 0 0 0 -1.5708 0 -1.5708 \
+       cam1_link camera_depth_optical_frame      __name:=tf_opt_cam1 &
+rosrun tf2_ros static_transform_publisher 0 0 0 -1.5708 0 -1.5708 \
+       cam2_link camera2_depth_optical_frame     __name:=tf_opt_cam2 &
 
 # 3) Reproduce los bags.  Ambos necesitan --clock
 rosbag play --clock -l record1.bag \
@@ -25,12 +37,15 @@ rosbag play --clock -l record2.bag \
 # 4) Nodelet manager y PointClouds
 rosrun nodelet nodelet manager __name:=nodelet_manager &
 sleep 1
+
 rosrun nodelet nodelet load depth_image_proc/point_cloud_xyz nodelet_manager \
-       __name:=pc_cam1 image_rect:=/cam1/depth/image_rect_raw \
-       camera_info:=/cam1/depth/camera_info points:=/cam1/points &
+       __name:=pc_cam1  image_rect:=/cam1/depth/image_rect_raw \
+       camera_info:=/cam1/depth/camera_info  points:=/cam1/points &
+
 rosrun nodelet nodelet load depth_image_proc/point_cloud_xyz nodelet_manager \
-       __name:=pc_cam2 image_rect:=/cam2/depth/image_rect_raw \
-       camera_info:=/cam2/depth/camera_info points:=/cam2/points &
+       __name:=pc_cam2  image_rect:=/cam2/depth/image_rect_raw \
+       camera_info:=/cam2/depth/camera_info  points:=/cam2/points \
+    output_frame_id:=camera2_depth_optical_frame & # <-- Lograr que el segundo bag tenga el frame correcto (ESTO NO FUNCIONA)
 
 # 5) Arranca RViz *después* de que /clock ya fluya
 sleep 2
