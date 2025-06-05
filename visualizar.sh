a41d29991a7b0abd55e601803ebfce86696c7562
Hengyi <hengyi2507@gmail.com>
2025-06-05 16:18:34 +0200

    Transformaciones de 2 posiciones ajustados

--- visualizar.sh
+++ visualizar.sh
@@ -12,18 +12,20 @@ cd rosbag_recordings
 # 1) Todos los nodos usarán el reloj del bag
 rosparam set use_sim_time true
 
-# 2) Publicamos las transformaciones estáticas
-rosrun tf2_ros static_transform_publisher 0 0 0 0 0 -1.5708 \
-       camera_link cam1_link   __name:=tf_cam1 &
-rosrun tf2_ros static_transform_publisher 1 0 0 3.1416 0 -1.5708 \
-       camera_link cam2_link   __name:=tf_cam2 &
+# 2) Publicamos las transformaciones estáticas , se pueden quitar estas tf
+# rosrun tf2_ros static_transform_publisher 0 0 0 1.5708 -1.5708 0 \
+#        camera_link cam1_link __name:=tf_cam1 &
+# rosrun tf2_ros static_transform_publisher 1 0 0 3.1416 0 -1.5708 \
+#        camera_link cam2_link   __name:=tf_cam2 &
 
-rosrun tf2_ros static_transform_publisher 0 0 0 -1.5708 0 -1.5708 \
-       cam1_link camera_depth_optical_frame      __name:=tf_opt_cam1 &
-rosrun tf2_ros static_transform_publisher 0 0 0 -1.5708 0 -1.5708 \
-       cam2_link camera2_depth_optical_frame     __name:=tf_opt_cam2 &
+# arreglar los frames id y luego los ejes de las camaras
 
-# 3) Reproduce los bags.  Ambos necesitan --clock
+rosrun tf2_ros static_transform_publisher 1 0 0 0 0 -1.5708 \
+      cam1_link camera_depth_optical_frame      __name:=tf_opt_cam1 &
+rosrun tf2_ros static_transform_publisher 0 0 1 0 3.1416 0 \
+      camera_depth_optical_frame camera2_depth_optical_frame     __name:=tf_opt_cam2 &
+
+# 3) Reproduce los bags.
 rosbag play --clock -l record1.bag \
     /camera/depth/image_rect_raw:=/cam1/depth/image_rect_raw \
     /camera/depth/camera_info:=/cam1/depth/camera_info \
@@ -44,9 +46,10 @@ rosrun nodelet nodelet load depth_image_proc/point_cloud_xyz nodelet_manager \
 
 rosrun nodelet nodelet load depth_image_proc/point_cloud_xyz nodelet_manager \
        __name:=pc_cam2  image_rect:=/cam2/depth/image_rect_raw \
-       camera_info:=/cam2/depth/camera_info  points:=/cam2/points \
-    output_frame_id:=camera2_depth_optical_frame & # <-- Lograr que el segundo bag tenga el frame correcto (ESTO NO FUNCIONA)
+       camera_info:=/cam2/depth/camera_info  points:=/cam2/points &
 
-# 5) Arranca RViz *después* de que /clock ya fluya
-sleep 2
+# 5) Arranca RViz después de que /clock ya fluya
+# source devel/setup.bash <-- dentro de .bashrc
+rosrun relay_tools relay_cam2.py &
+
 rviz   # en Global Options fija “Fixed Frame” a cam1_link (o cam2_link)
